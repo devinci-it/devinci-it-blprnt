2970f3619c9818485753ea7e8aa24f4f4766412a
www-data <www-data@hkbx.local>
2026-04-22 20:12:18 -0700

    feat(bootstrap): streamline CLI bootstrap process and add API/console helper functions fix(index): enhance session security settings and improve error handling feat(Response): add security headers to JSON response

--- src/Core/Response.php
+++ src/Core/Response.php
@@ -7,6 +7,12 @@ class Response
     {
         http_response_code($code);
         header('Content-Type: application/json');
+        // Security headers
+        header('Permissions-Policy: interest-cohort=()');
+        header('Referrer-Policy: no-referrer');
+        header('Strict-Transport-Security: max-age=63072000; includeSubDomains; preload');
+        header('Content-Security-Policy: default-src \'self\'; script-src \'self\'; object-src \'none\';');
+        header('X-Content-Type-Options: nosniff');
         echo json_encode($data);
     }
 }
